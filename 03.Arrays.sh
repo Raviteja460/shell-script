4cc450e3490335216a9f0a6b3b977174877d8099
Raviteja460 <seelamsettyraviteja@gmail.com>
2023-12-13 20:51:26 +0530

    New Script1234

---
+++ 03.Arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+CINEMAHALLS=("PVR" "IMAX" "AA")
+
+echo "The good cinemahall is: ${CINEMAHALLS[0]}"
+
+echo "The nearest cinemahall is: ${CINEMAHALLS[1]}"
+
+echo "${CINEMAHALLS[2]} :is present in HYDERABAD"
