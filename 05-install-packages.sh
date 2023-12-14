b264b0645c8731e56ae82df1a9b3ee29f1607115
Raviteja460 <seelamsettyraviteja@gmail.com>
2023-12-14 17:30:51 +0530

    New Script1234

--- 05-install-packages.sh
+++ 05-install-packages.sh
@@ -12,7 +12,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script started at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    if[ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2 ... $R FAILED $N"
     else
@@ -33,8 +33,8 @@ echo "All arguments passed: $@"
 for package in $@
 do 
     yum list installed $package &>> $LOGFILE
-    if[ $? -ne 0 ]
-    then 
+    if [ $? -ne 0 ]
+    then
         yum install $package -y &>> $LOGFILE
         VALIDATE $? "Installation of $package"
     else
