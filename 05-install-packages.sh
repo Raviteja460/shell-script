5454493657c5a4522ff0be2e708e2f0bd4f4af42
Raviteja460 <seelamsettyraviteja@gmail.com>
2023-12-14 16:59:45 +0530

    New Script1234

--- 05-install-packages.sh
+++ 05-install-packages.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-    echo "Please run this script with root access"
+    echo " $R ERROR:: Please run this script with root access $N"
     exit 1
 else
-    echo "You are root user"
+    echo "$G You are root user $N"
 fi
